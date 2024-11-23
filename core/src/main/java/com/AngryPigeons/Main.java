e205624d442918f2c7f5c655f903209452da3f18
RamanujanGopalan <ramanujan23424@iiitd.ac.in>
2024-11-23 19:02:31 +0530

    Added level 3 to game

--- core/src/main/java/com/AngryPigeons/Main.java
+++ core/src/main/java/com/AngryPigeons/Main.java
@@ -51,6 +51,7 @@ public class Main extends Game {
 
         levelInfoList.add(new LevelInfo("Maps/AP_TestLevelMap.tmx", new ArrayList<>(List.of(1,2,3))));
         levelInfoList.add(new LevelInfo("Maps/AP_TestLevelMap2.tmx", new ArrayList<>(List.of(1,2,1,3))));
+        levelInfoList.add(new LevelInfo("Maps/AP_TestLevelMap3.tmx", new ArrayList<>(List.of(1,2,1,3))));
 
 //        LevelScreen levelScreen1 = new LevelScreen("Maps/AP_TestLevelMap.tmx", new ArrayList<>(List.of(1,3,1)));
 //        LevelScreen levelScreen1 = new LevelScreen(levelInfoList.get(0));
